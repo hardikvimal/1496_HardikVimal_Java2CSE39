0f944e2c12b252336bb48d700efdf5a38dc6e50b
Hardik Vimal <hardik.25scs1003001496@iilm.edu>
2026-08-22 23:42:19 +0530

    Add files via upload

---
+++ Assignment_5/MultiplesOfFive.java
@@ -0,0 +1,8 @@
+public class MultiplesOfFive {
+    public static void main(String[] args) {
+
+        for (int i = 5; i <= 100; i = i + 5) {
+            System.out.println(i);
+        }
+    }
+}
